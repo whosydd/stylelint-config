3780075fd4f81936eede3e7867dd772f178bf851
whosydd <dev.youngkwok718@gmail.com>
2021-07-19 15:44:20 +0800

    1.1.0

--- README.md
+++ README.md
@@ -24,20 +24,16 @@
 ```json
 // settings.json:
 "stylelint-config.gist": {
-    "configUrl": "https://gist.githubusercontent.com/whosydd/6f648ed0e1ea6ca3f8daeca6df41a788/raw/42b1242eb2e9a6989b98693475b28557324ed95f/.stylelintrc.js",
-    // 如果不想生成 .stylelintignore 文件，请不要设置 ignoreUrl
-    // If you do not want to generate a .stylelintignore file, please do not set 'ignoreUrl'
-    "ignoreUrl": "https://gist.githubusercontent.com/whosydd/8cbac80a0344d0571f30f51a7562034c/raw/4dbf6844aeb30a0e175b69e8babae82ec50e2e78/.stylelintignore"
+    "configID": "6f648ed0e1ea6ca3f8daeca6df41a788",
+    // 如果不想生成 .stylelintignore 文件，请不要设置 ignoreID
+    // If you do not want to generate a .stylelintignore file, please do not set 'ignoreID'
+    "ignoreID": "8cbac80a0344d0571f30f51a7562034c"
 },
 ```
 
-可以选择在[gist](https://gist.github.com/)上添加`.stylelintrc.js`以及`.stylelintignore`，然后通过`wget`下载到本地
+可以选择在[gist](https://gist.github.com/)上添加`.stylelintrc.js`以及`.stylelintignore`，将`gist_id`添加到配置项中，扩展会通过`ajax`请求获取相应配置文件
 
-**注意**：如果你不想生成 `.stylelintignore` 文件，请不要设置` ignoreUrl`
-
-**获取下载地址：**
-
-![Screenshot of StylelintConfig for VS Code](https://raw.githubusercontent.com/whosydd/images-in-one/main/20210712130147.png)
+#### 获取`gist_id`:![Capture](https://raw.githubusercontent.com/whosydd/images-in-one/main/20210719143245.PNG)
 
 ### Tip
 
@@ -50,8 +46,6 @@
 # You can choose to install dependencies manually
 npm i -D stylelint stylelint-config-standard stylelint-config-recess-order stylelint-config-prettier
 ```
-
-
 
 设置为`false`时，将不再提示`install dependencies`![Capture](https://raw.githubusercontent.com/whosydd/images-in-one/main/20210712125946.PNG)
 
--- extension.js
+++ extension.js
@@ -1,89 +1,100 @@
 const vscode = require('vscode')
 const fs = require('fs')
-
+const axios = require('axios')
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+  // 通知
+  vscode.window.showInformationMessage('StylelintConfig for VS Code 1.1.0 NEW!', {
+    modal: true,
+    detail: `由于改用ajax请求的方式获取gist上的配置文件，现在配置项需要gist_id，请参考扩展详情页！
+    
+    Please refer to the extension details page!`,
+  })
+
   let stylelint = vscode.commands.registerCommand('stylelint-config', async function (folder) {
     // 获取配置项
     const flag = await vscode.workspace.getConfiguration('stylelint-config').get('tip')
     const res = await vscode.workspace.getConfiguration('stylelint-config').get('gist')
-    const { configUrl, ignoreUrl } = res
+    const { configID, ignoreID } = res
 
     // 获取工作区路径
     let workspace
-    if (!folder) {
-      workspace = vscode.workspace.workspaceFolders[0].uri.fsPath
-    } else workspace = folder.fsPath
+    if (!folder) workspace = vscode.workspace.workspaceFolders[0].uri.fsPath
+    else workspace = folder.fsPath
 
-    // 复制源文件
+    // default
+    const defaultConfigFile = fs.readFileSync(`${__dirname}/src/.stylelintrc.js`, 'utf-8')
+    const defaultIgnoreFile = fs.readFileSync(`${__dirname}/src/.stylelintignore`, 'utf-8')
+
+    // handle
     function copyHandle() {
-      fs.writeFileSync(
-        `${workspace}/.stylelintrc.js`,
-        fs.readFileSync(`${__dirname}/src/.stylelintrc.js`, 'utf-8')
-      )
-      fs.writeFileSync(
-        `${workspace}/.stylelintignore`,
-        fs.readFileSync(`${__dirname}/src/.stylelintignore`, 'utf-8')
-      )
+      // 判断配置项
+      if (configID) {
+        axios
+          .get('https://api.github.com/gists/' + configID)
+          .then(res => {
+            const configFile = res.data.files['.stylelintrc.js'].content
+            fs.writeFileSync(`${workspace}/.stylelintrc.js`, configFile)
+            tip()
+          })
+          .catch(err => {
+            vscode.window.showErrorMessage(err.message)
+          })
+        if (ignoreID) {
+          axios
+            .get('https://api.github.com/gists/' + ignoreID)
+            .then(res => {
+              const ignoreFile = res.data.files['.stylelintignore'].content
+              fs.writeFileSync(`${workspace}/.stylelintignore`, ignoreFile)
+              tip()
+            })
+            .catch(err => {
+              vscode.window.showErrorMessage(err.message)
+            })
+        }
+      } else {
+        fs.writeFileSync(`${workspace}/.stylelintrc.js`, defaultConfigFile)
+        fs.writeFileSync(`${workspace}/.stylelintignore`, defaultIgnoreFile)
+        tip()
+      }
+    }
+
+    // tip
+    function tip() {
       if (!flag) return
       vscode.window
-        .showInformationMessage('Do you need to install dependencies?', 'Install', 'Already Done')
+        .showInformationMessage('Do you need to install stylelint?', 'Install', 'Already Done')
         .then(answer => {
           if (answer === 'Install') {
             const terminal = vscode.window.createTerminal({
               name: 'stylelint',
-              // hideFromUser: true,
             })
             terminal.show()
-            tip(terminal)
+            try {
+              terminal.sendText(
+                `npm i -D stylelint stylelint-config-standard stylelint-config-recess-order stylelint-config-prettier`
+              )
+            } catch (err) {
+              vscode.window.showErrorMessage(
+                `请手动安装依赖！"npm i -D stylelint stylelint-config-standard stylelint-config-recess-order stylelint-config-prettier"`
+              )
+            }
           }
         })
     }
-    // 从gist动态获取
-    function wgetHandle(configUrl, ignoreUrl) {
-      const terminal = vscode.window.createTerminal({
-        name: 'stylelint gist',
-      })
-      terminal.show()
-      try {
-        if (ignoreUrl) terminal.sendText(`wget ${ignoreUrl} -O .stylelintignore`)
-        terminal.sendText(`wget ${configUrl} -O .stylelintrc.js`)
-      } catch (error) {
-        vscode.window.showWarningMessage(error)
-      }
-      if (!flag) return
-      vscode.window
-        .showInformationMessage('Do you need to install dependencies?', 'Install', 'Already Done')
-        .then(answer => {
-          if (answer === 'Install') tip(terminal)
-        })
-    }
 
-    // 安装依赖
-    function tip(terminal) {
-      try {
-        terminal.sendText(
-          `npm i -D stylelint stylelint-config-standard stylelint-config-recess-order stylelint-config-prettier`
-        )
-      } catch (err) {
-        vscode.window.showErrorMessage(
-          `请手动安装依赖！"npm i -D stylelint stylelint-config-standard stylelint-config-recess-order stylelint-config-prettier"`
-        )
-      }
-    }
-
+    // 判断工作区是否存在配置文件
     if (
       !fs.existsSync(
         `${workspace}/.stylelintrc.js` ||
           `${workspace}/.stylelintrc` ||
           `${workspace}/.stylelintrc.json`
       )
-    ) {
-      // 判断配置项
-      configUrl ? wgetHandle(configUrl, ignoreUrl) : copyHandle()
-    } else {
+    )
+      copyHandle()
+    else
       vscode.window
         .showWarningMessage(
           'An .stylelintrc file already exists in this workspace.',
@@ -91,14 +102,12 @@ function activate(context) {
           'OK'
         )
         .then(value => {
-          value === 'Replace' ? (configUrl ? wgetHandle(configUrl, ignoreUrl) : copyHandle()) : null
+          value === 'Replace' ? copyHandle() : null
         })
-    }
   })
 
   context.subscriptions.push(stylelint)
 }
-
 function deactivate() {}
 
 module.exports = {
